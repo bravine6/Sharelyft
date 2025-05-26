d390b78c672c5cbda3098e6fd88bdf66f18cf350
Bravine Cheruiyot <ropbravine@gmail.com>
2025-05-26 13:00:39 +1000

    Fix Cypress config and add wait-on for Jenkins pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/integration/**/*.js',
     baseUrl: "http://localhost:3000",
     supportFile: false,
   },
