cbdc375b39f7f58eb06b2b22cfaacfc9f09de8d5
Bravine Cheruiyot <ropbravine@gmail.com>
2025-05-26 16:21:59 +1000

    More changes on the security phase to try and solve for elisnt error

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2020,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-console": "off",
+    "no-unused-vars": "warn",
+    "no-undef": "warn"
+  }
+};
